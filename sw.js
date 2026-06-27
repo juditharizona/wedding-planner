7990a10a429edcfe11eca9066f36b2817c8cd83a
juditharizona <juditharizona22@gmail.com>
2026-06-27 20:53:51 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'wedding-planner-v1';
+const FILES = [
+  '/wedding-planner/',
+  '/wedding-planner/index.html',
+  '/wedding-planner/manifest.json',
+  '/wedding-planner/icon-192.png',
+  '/wedding-planner/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
